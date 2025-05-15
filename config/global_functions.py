b3aa144ef2a35049bc833bd85a3a9383b6ceedf2
george <george@seamlessai.ntesmail.com>
2025-05-15 17:59:57 +0800

    dynamic workflow

---
+++ config/global_functions.py
@@ -0,0 +1,9 @@
+def is_planner_needed(has_lauched: bool) -> bool:
+    if has_lauched:
+        return False
+    else:
+        return True
+
+func_map = {
+    "is_planner_needed": is_planner_needed
+}
---
+++ config/global_variables.py
@@ -0,0 +1,5 @@
+
+
+context_variables = {
+    "has_lauched": False
+}
